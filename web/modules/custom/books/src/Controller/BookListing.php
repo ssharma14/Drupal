6ca484d41addddb8fb279ee60c1cc9cbb40bbb17
shrishti sharma <sshrishti14@gmail.com>
2022-11-05 02:14:06 -0600

    added api function using js

--- web/modules/custom/books/src/Controller/BookListing.php
+++ web/modules/custom/books/src/Controller/BookListing.php
@@ -6,14 +6,15 @@ use Drupal\Core\Controller\ControllerBase;
 
 
 class BookListing extends ControllerBase {
- 
-  /**
-   *   Method to render content/data to page
-   */
+
   public function view() {
-    return [
-        '#markup' => $this->t('My custom page'),
-    ];
+      return[
+          '#theme' => 'book-listing',
+          '#attached' => [
+            'library' => [
+                'books/general',
+            ],
+          ],
+      ];
   }
- 
 }
